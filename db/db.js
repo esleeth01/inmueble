a68c2e352de1c8b4ac0237176da740c00ff7aec7
esleethPC <esleeth.carol@gmail.com>
2023-11-19 22:03:22 -0500

    se transforma db.js en una clase

--- db/db.js
+++ db/db.js
@@ -1,22 +1,37 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// Conectar a la base de datos (o crearla si no existe)
-const db = new sqlite3.Database('db/el_cachaco.db');
+class Database {
+    constructor(dbPath) {
+        this.db = new sqlite3.Database(dbPath);
+        this.init();
+    }
 
-// Crear una tabla de usuarios
-db.serialize(() => {
-    db.run('CREATE TABLE IF NOT EXISTS usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nombres TEXT, apellidos TEXT, usuario TEXT, correo TEXT, contraseña TEXT)');
+    init() {
+        // Crea la tabla si no existe
+        this.db.run(`
+            CREATE TABLE IF NOT EXISTS usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombres TEXT,
+                apellidos TEXT,
+                usuario TEXT,
+                correo TEXT,
+                contraseña TEXT
+            )
+        `);
+    }
 
-    // Insertar un usuario de ejemplo
-    const stmt = db.prepare('INSERT INTO usuarios (nombres, apellidos, usuario, correo, contraseña) VALUES (?, ?, ?, ?, ?)');
-    stmt.run('John', 'Doe', 'john_doe', 'john@example.com', 'hashed_password');
-    stmt.finalize();
+    insertUsuario(nombres, apellidos, usuario, correo, contraseña) {
+        const stmt = this.db.prepare(`
+            INSERT INTO usuarios (nombres, apellidos, usuario, correo, contraseña)
+            VALUES (?, ?, ?, ?, ?)
+        `);
+        stmt.run(nombres, apellidos, usuario, correo, contraseña);
+        stmt.finalize();
+    }
 
-    // Consultar y mostrar todos los usuarios
-    db.each('SELECT * FROM usuarios', (err, row) => {
-        console.log(`${row.id}: ${row.nombres} ${row.apellidos} - ${row.usuario} - ${row.correo} - ${row.contraseña}`);
-    });
-});
+    close() {
+        this.db.close();
+    }
+}
 
-// Cerrar la conexión a la base de datos al finalizar
-db.close();
+module.exports = Database;
